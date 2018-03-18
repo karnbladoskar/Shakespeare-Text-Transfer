aa5f371fc319c5aed1dd92137483b22db7de5896
Oskar Karnblad <oskar@Oskars-MacBook-Pro-2.local>
2018-03-18 12:44:12 +0100

    Response added to gui

--- gui/single-view/main.py
+++ gui/single-view/main.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox, QLabel, QTabWidget, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox, QLabel, QTabWidget, QVBoxLayout, QTextEdit, QGridLayout
 from PyQt5.QtGui import QIcon, QPixmap, QFont
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot, Qt
 sys.path.append('../../')
 import restore_char_model as rcm
 
@@ -24,23 +24,32 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-            # Create widget
+        # Create widget
         label = QLabel(self)
         pixmap = QPixmap('../shakespeare.jpg')
         label.setPixmap(pixmap)
         label.resize(pixmap.width(), pixmap.height())
 
+        # Create textbox
+        self.textbox = QLineEdit(self)
+        self.textbox.move(20, 20)
+        self.textbox.resize(200, 30)
 
-            # Create textbox
-        textbox = QLineEdit(self)
-        textbox.move(20, 20)
-        textbox.resize(200, 30)
-
-            # Create button
+        # Create button
         shake_button = QPushButton('Shake it!', self)
         shake_button.setToolTip('Lets get shakey')
         shake_button.move(220, 20)
         shake_button.clicked.connect(self.on_shake)
+
+        returnText = QTextEdit()
+        returnText.setReadOnly(True)
+
+        self.mainLayout = QGridLayout()
+        self.mainLayout.addWidget(self.textbox, 0, 0)
+        self.mainLayout.addWidget(shake_button, 0, 1)
+        self.mainLayout.addWidget(returnText, 1, 1)
+
+        self.setLayout(self.mainLayout)
 
         self.show()
 
@@ -50,6 +59,11 @@ class App(QWidget):
         self.user_input = self.textbox.text()
         shaked_output = self.make_model_prediction()
 
+        returnText = QTextEdit(shaked_output)
+        self.mainLayout.addWidget(returnText, 1, 1)
+        self.setLayout(self.mainLayout)
+
+        self.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
